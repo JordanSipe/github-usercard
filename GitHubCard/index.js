fe8f27d7e3a4c6e0a9badcf273f38e990c4c154a
JordanSipe <jsipe2017@gmail.com>
2021-05-06 18:00:38 -0400

    Finished Project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,12 @@
     https://api.github.com/users/<your name>
 */
 
+import axios from "axios"
+const cards = document.querySelector('.cards');
+
+  axios
+    .get("https://api.github.com/users/JordanSipe")
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -17,6 +23,20 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+  function followerProfile(userProfile) {
+    axios
+      .get(`https://api.github.com/users/${userProfile}`)
+      .then((res) => {
+        const userData = res.data;
+        const card = cardMaker(userData);
+        return cards.appendChild(card);
+      })
+      .catch((error) => {
+        console.log(`Here's where you messed up: ${error}`)
+      })
+    }
+
+    followerProfile('JordanSipe');
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,12 +48,63 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+    followersArray.forEach(followerProfile);
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
+*/
+    function cardMaker(cardObj) {
+    const card = document.createElement('div');
+    const Img = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const name = document.createElement('h3');
+    const username = document.createElement('p');
+    const location = document.createElement('p');
+    const profile = document.createElement('p');
+    const address = document.createElement('a');
+    const followers = document.createElement('p');
+    const following = document.createElement('p');
+    const bio = document.createElement('p');
 
+    card.appendChild(Img);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(username);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+    profile.appendChild(address);
+    
+
+    card.classList.add('card');
+    cardInfo.classList.add('card-info');
+    name.classList.add('name');
+    username.classList.add('username');
+
+    Img.src = cardObj.avatar_url;
+    name.textContent = cardObj.name;
+    username.textContent = cardObj.login;
+    location.textContent = cardObj.location;
+    profile.textContent = cardObj.html_url;
+    address.textContent = cardObj.url;
+    address.href = cardObj.url;
+    followers.textContent = cardObj.followers;
+    following.textContent = cardObj.following;
+    bio.textContent = cardObj.bio;
+
+    
+
+    return card;
+    }
+
+
+    
+/*
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
